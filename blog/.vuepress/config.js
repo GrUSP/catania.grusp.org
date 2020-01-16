3f5ce524c8b3c84fb8def024839a7dd3b7bbf031
sergio sicari <sergiosicari@gmail.com>
2020-01-16 22:16:12 +0100

    changing with vuepress

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,131 @@
+module.exports = {
+  // Title of your website
+  title: 'PUG Catania',
+
+  // Description of your website
+  description: 'Php User Group Catania',
+
+  // Language of your website
+  locales: {
+    '/': {
+      lang: 'en-US',
+    },
+  },
+
+  // Theme to use
+  theme: 'meteorlxy',
+
+  // Theme config
+  themeConfig: {
+    // Language of this theme. See the [Theme Language] section below.
+    lang: 'en-US',
+
+    // Personal infomation (delete the fields if you don't have / don't want to display)
+    personalInfo: {
+      // Nickname
+      nickname: 'PUG Catania',
+
+      // Introduction of yourself (HTML supported)
+      description: 'Happy Coding',
+
+      // Email
+      email: '',
+
+      // Your location
+      location: 'Via Messina 609, Catania, Italia',
+
+      // Your organization
+      organization: '',
+
+      // Your avatar image
+      // Set to external link
+      avatar: '/assets/img/pug-catania-1.jpg',
+      // Or put into `.vuepress/public` directory. E.g. `.vuepress/public/img/avatar.jpg`
+      // avatar: '/img/avatar.jpg',
+
+      // Accounts of SNS
+      sns: {
+        // Github account and link
+        github: {
+          account: 'meteorlxy',
+          link: 'https://github.com/meteorlxy',
+        },
+
+        // Facebook account and link
+        facebook: {
+          account: 'meteorlxy.cn',
+          link: 'https://www.facebook.com/meteorlxy.cn',
+        },
+      },
+    },
+
+    // Header Config (Optional)
+    header: {
+      // The background of the header. You can choose to use an image, or to use random pattern (geopattern)
+      background: {
+        // URL of the background image. If you set the URL, the random pattern will not be generated, and the `useGeo` will be ignored.
+        url: '',
+
+        // Use random pattern. If you set it to `false`, and you don't set the image URL, the background will be blank.
+        useGeo: true,
+      },
+
+      // show title in the header or not
+      showTitle: true,
+    },
+
+    // Footer Config (Optional)
+    footer: {
+      // Show 'Powered by VuePress' or not (enable it to support VuePress)
+      poweredBy: true,
+
+      // Show the theme that you are using (enable it to support this theme) (please do not disable it, haha)
+      poweredByTheme: true,
+
+      // Add your custom footer (HTML supported)
+      custom: 'Copyright 2018-present <a href="https://github.com/meteorlxy" target="_blank">meteorlxy</a> | MIT License',
+    },
+
+    // Info Card Config (Optional)
+    infoCard: {
+      // The background of the info card's header. You can choose to use an image, or to use random pattern (geopattern)
+      headerBackground: {
+        // URL of the background image. If you set the URL, the random pattern will not be generated, and the `useGeo` will be ignored.
+        url: '/assets/img/bg',
+
+        // Use random pattern. If you set it to `false`, and you don't set the image URL, the background will be blank.
+        useGeo: true,
+      },
+    },
+
+    // Show the last updated time of your posts
+    lastUpdated: true,
+
+    // The content of your navbar links
+    nav: [
+      { text: 'Home', link: '/', exact: true },
+      { text: 'Posts', link: '/posts/', exact: false },
+    ],
+
+    // Comments config. See the [Posts Comments] section below.
+    //comments: {
+    //  owner: 'meteorlxy',
+    //  repo: 'vuepress-theme-meteorlxy',
+    //  clientId: 'MY_CLIENT_ID',
+    //  clientSecret: 'MY_CLIENT_SECRET',
+    //},
+
+    // Pagination config (Optional)
+    pagination: {
+      perPage: 5,
+    },
+
+    // Default Pages (Optional, the default value of all pages is `true`)
+    defaultPages: {
+      // Allow theme to add Home page (url: /)
+      home: true,
+      // Allow theme to add Posts page (url: /posts/)
+      posts: true,
+    },
+  },
+}
---
+++ blog/_posts/2020-01-16-sylius-and-conventional-commit.md
@@ -0,0 +1,35 @@
+---
+top: true
+layout: post
+title:  "Il Meetup Di Gennaio - Sylius & Conventional Commits"
+date:   2020-01-16 21:30 +0100
+category: post
+tags:
+    - events
+header-image: /assets/img/php-74-release-party.jpeg
+excerpt_separator: <!--more-->
+vssue: false
+---
+
+![alt text](/assets/img/meetup-gennaio-pug-catania.jpeg "Meetup di gennaio 2020 - Sylius & Conventional Commits")
+
+L'appuntamento è per `Martedì 28/01/2020` alle ore `18:00`
+
+Vedremo insieme a Sergio Sicari come mettere in piedi un e-commerce usando `Symfony` e `Sylius`, una soluzione molto flessibile per implementare e-commerce su Symfony 🛒
+
+Architettura, installazione e "customizzazioni" saranno i punti principali di questo primo talk.
+
+Nel secondo talk invece Dario Finocchiaro ci parlerà dei `conventional commits`, ovvero di come usare al meglio il sistema di versioning `GIT`.
+<!--more-->
+
+Questi sono gli argomenti che tratteremo nel secondo appuntamento del PUG Catania 🐘
+
+L'ingresso è come sempre gratuito ma i posti sono limitati quindi affrettati a prendere il tuo biglietto 😎
+
+AGENDA
+
+18:30 - Sylius: E-commerce 4 Symfony (Sergio Sicari)
+19:00 - Git & Conventional Commit (Dario Finocchiaro)
+19:30 - Q&A e networking
+
+👉 [Link al Meetup](https://www.meetup.com/it-IT/pugCatania-PHP-User-Group-Catania/events/267776543/)
